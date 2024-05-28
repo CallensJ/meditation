f1c2a575889d7941b323ec158920647e4c5df095
CallensJ <callens.johan@protonmail.com>
2024-05-28 10:49:03 +0800

    first commit

---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,11 @@
+import styles from './Homepage.module.css';
+
+export default function Homepage() {
+  return (
+    <main className={styles.homepage}>
+        <section>
+            
+        </section>
+    </main>
+  )
+}
