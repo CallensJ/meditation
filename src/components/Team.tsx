33a64a337141b1fc8cc13fd14d58415b72af1d38
CallensJ <callens.johan@protonmail.com>
2024-06-17 15:28:33 +0200

    responsive team section

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -7,28 +7,28 @@ export default function Team() {
         <h2 className={styles.sectionTitle}>L'équipe</h2>
         <div className={styles.bloc}>
           <div className={styles.card}>
-            <img src="../img5.jpg" alt="nom prenom" className={styles.avatar} />
-            <h4>Nom Prenom</h4>
-            <p>occupation</p>
+            <img src="../naomi.jpeg" alt="nom prenom" className={styles.avatar} />
+            <h4>Naomi</h4>
+            <p>Soins & Massages</p>
           </div>
 
           <div className={styles.card}>
-            <img src="../img5.jpg" alt="nom prenom" className={styles.avatar} />
-            <h4>Nom Prenom</h4>
-            <p>occupation</p>
+            <img src="../sebastien.jpeg" alt="sebastien" className={styles.avatar} />
+            <h4>Sebastien</h4>
+            <p>Yoga & massages</p>
           </div>
         </div>
 
         <div className={styles.bloc}>
           <div className={styles.card}>
-            <img src="../img5.jpg" alt="nom prenom" className={styles.avatar} />
-            <h4>Nom Prenom</h4>
-            <p>occupation</p>
+            <img src="../Laura.png" alt="nom prenom" className={styles.avatar} />
+            <h4>Laura</h4>
+            <p>Guerison, reiki</p>
           </div>
           <div className={styles.card}>
-            <img src="../img5.jpg" alt="nom prenom" className={styles.avatar} />
-            <h4>Nom Prenom</h4>
-            <p>occupation</p>
+            <img src="../Jessica.jpg" alt="nom prenom" className={styles.avatar} />
+            <h4>Jessica</h4>
+            <p>Yoga & bien etre</p>
           </div>
         </div>
       </div>
