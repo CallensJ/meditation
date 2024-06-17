ee073d8033dfb1bf4f20238bb89f9880c95e3ab3
CallensJ <callens.johan@protonmail.com>
2024-06-17 16:17:09 +0200

    responsive footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
-import { CiLocationOn } from "react-icons/ci";
+import { CiClock1, CiLocationOn, CiMail } from "react-icons/ci";
 import styles from "./Footer.module.css";
+import { FaRegAddressBook } from "react-icons/fa";
 
 
 export default function Footer() {
@@ -7,11 +8,17 @@ export default function Footer() {
     <footer>
       <div className={styles.bloc}>
         <div className={styles.bloc1}>
-        <CiLocationOn />
+          <div>
+        <CiLocationOn className={styles.icon}/>
           <span>
           
             +33466853815</span>
+
+          </div>
+          <div>
+          <CiMail className={styles.icon}/>
           <span>contact@harmonya.com</span>
+          </div>
         </div>
       </div>
       <div className={styles.bloc}>
@@ -22,8 +29,15 @@ export default function Footer() {
       </div>
       <div className={styles.bloc}>
         <div className={styles.bloc3}>
+          <div>
+          <FaRegAddressBook className={styles.icon}/>
           <span>5 rue des oasis, 48000 Lozère</span>
+
+          </div>
+          <div>
+          <CiClock1 className={styles.icon}/>
           <span>8h00 - 20h00 </span>
+          </div>
         </div>
       </div>
     </footer>
