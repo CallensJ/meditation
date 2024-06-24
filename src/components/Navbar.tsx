d8af7b26233a497eecee0bb0646ed09c00cd4d3e
CallensJ <callens.johan@protonmail.com>
2024-06-24 12:31:02 +0200

    added responsive introduction section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import { Icon } from 'react-icons-kit';
-import { menu } from 'react-icons-kit/feather/menu';
-import { x } from 'react-icons-kit/feather/x';
-import styles from './Navbar.module.css';
-import Logo from './Logo';
-import { NavLink } from 'react-router-dom';
-import classNames from 'classnames';
+import React, { useState } from "react";
+import { Icon } from "react-icons-kit";
+import { menu } from "react-icons-kit/feather/menu";
+import { x } from "react-icons-kit/feather/x";
+import styles from "./Navbar.module.css";
+import Logo from "./Logo";
+import { NavLink } from "react-router-dom";
+import classNames from "classnames";
 
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -23,7 +23,7 @@ const Navbar: React.FC = () => {
         <div className={styles.menuIcon} onClick={toggleNavbar}>
           <Icon icon={isOpen ? x : menu} size={24} />
         </div>
-        <ul className={`${styles.navMenu} ${isOpen ? styles.active : ''}`}>
+        <ul className={`${styles.navMenu} ${isOpen ? styles.active : ""}`}>
           <li className={styles.navItem}>
             <NavLink
               to="/"
@@ -37,7 +37,7 @@ const Navbar: React.FC = () => {
           <li className={styles.navItem}>
             <NavLink
               to="/about"
-              className={({ isActive }) =>
+              className={({ isActive }): string =>
                 classNames(styles.navLinks, { [styles.active]: isActive })
               }
             >
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -6,6 +6,7 @@ import Banner from "../components/Banner";
 import Footer from "../components/Footer";
 import Team from "../components/Team";
 import Gallery from "../components/Gallery";
+import Introduction from "../components/Introduction";
 
 
 
@@ -20,32 +21,7 @@ const HomePage: React.FC = () => {
         </div>
       </section>
 
-      <section className={styles.introduction}>
-        <div className={styles.textContainer}>
-          <h2 className={styles.title}>Harmonya</h2>
-          <p>
-            La méditation et la guérison par les sons sont des pratiques
-            ancestrales qui exploitent le pouvoir du son et de la pleine
-            conscience pour promouvoir le bien-être mental, émotionnel et
-            physique.
-          </p>
-          <p>
-            La méditation implique de concentrer l'esprit et de cultiver un état
-            de conscience détendue, souvent à travers des techniques telles que
-            la respiration profonde, la visualisation et la répétition de
-            mantras. La guérison par les sons, quant à elle, utilise les
-            vibrations d'instruments comme les bols chantants, les gongs et les
-            diapasons pour résonner avec les champs énergétiques du corps,
-            facilitant un état de relaxation profonde et de guérison.
-          </p>
-          <p>
-            {" "}
-            Ensemble, ces pratiques peuvent aider à réduire le stress, améliorer
-            la clarté mentale et favoriser un sentiment de paix intérieure, en
-            faisant des outils puissants pour le bien-être holistique.
-          </p>
-        </div>
-      </section>
+      <Introduction />
       <Gallery />
       <Banner />
       <Me />
